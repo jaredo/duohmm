fc49ca1f9da1bd727eaacb0fa2cc12135216d0fb
Jared O'Connell <joconnell@illumina.com>
2014-08-25 15:17:10 +0100

    updated makefile + version
    Former-commit-id: 37eb1eb91d6230b78295b27d69eef91f88e5a0d4

--- src/duohmm.cpp
+++ src/duohmm.cpp
@@ -16,7 +16,7 @@
 #include <boost/program_options.hpp>
 namespace po = boost::program_options;
 
-#define version "2014.08.21.0"
+#define version "2014.08.25.1"
 using namespace std;
 
 int main(int argc,char **argv) {
